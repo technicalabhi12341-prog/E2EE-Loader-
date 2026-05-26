c6831a5651fb95a6131e13b03b950ef0781fc436
technicalabhi12341-prog <technicalabhi12341@gmail.com>
2026-05-27 02:29:26 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -9,7 +9,7 @@ async function premiumLoadKeys() {
     try {
 
         const response = await fetch(
-"https://technicalabhi12341-prog.github.io/E2EE-Loader-/keys.json?t=" + Date.now()
+    "https://technicalabhi12341-prog.github.io/E2EE-Loader-/keys.json?t=" + Date.now()
 );
 
 const keys = await response.json();
